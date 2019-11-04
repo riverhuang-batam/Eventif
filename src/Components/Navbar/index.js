6b39efbe83c138cba90b81ce437248fe20eab522
riverhuang-batam <riverhuang011@gmail.com>
2019-11-04 15:46:19 +0700

    test

--- src/Components/Navbar/index.js
+++ src/Components/Navbar/index.js
@@ -1,7 +1,7 @@
 import React,{Component} from 'react';
 import {Nav, Navbar,NavbarBrand, NavbarToggler,NavItem, UncontrolledDropdown,DropdownMenu, DropdownItem, Collapse, Jumbotron, Card, Row, Col } from 'reactstrap'
 import {Route, Switch, HashRouter, NavLink} from 'react-router-dom'
-import Mainpages from './../../pages/Mainpages'
+
 import Eventpages from './../../pages/Eventpages'
 import Loginpages from './../../pages/Loginpages'
 import Profilepages from '../../pages/Profilepages'
@@ -57,7 +57,7 @@ render() {
 
    </div>
    <Switch>
-     <Route exact path="/" component={Mainpages}/>
+     
      <Route exact path="/Eventpages" component={Eventpages}/>
      <Route exact path="/Loginpages" component={Loginpages}/>
      <Route exact path="/Profilepages" component={Profilepages}/>
