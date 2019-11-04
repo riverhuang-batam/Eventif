eb2237292767a13d567c4a5cfc6f214a08f3682f
riverhuang-batam <riverhuang011@gmail.com>
2019-11-04 15:33:17 +0700

    test

--- src/pages/MainPages/index.js
+++ src/pages/MainPages/index.js
@@ -1,6 +1,4 @@
 import React,{Component} from 'react'
-import {Container} from 'reactstrap'
-import Navbars from './../../Components/Navbar'
 import JumbotronMainpages from '../../Components/Jumbotronmainpages'
 export default class Mainpages extends Component{
     render(){
